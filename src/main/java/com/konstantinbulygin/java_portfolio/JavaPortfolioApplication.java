34a593e480124e07c50177cf768e42347f10ff5d
kostyaFrom <korustlt@gmail.com>
2021-02-08 20:52:26 +0400

    init commit

---
+++ src/main/java/com/konstantinbulygin/java_portfolio/JavaPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.konstantinbulygin.java_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/konstantinbulygin/java_portfolio/JavaPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konstantinbulygin.java_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
